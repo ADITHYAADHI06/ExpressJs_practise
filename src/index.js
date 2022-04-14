c7ad35e1bbbb75acb218e421c8e2002f3e483153
ADITHYAADHI06 <adishettygu102@gmail.com>
2022-04-14 22:37:45 +0530

    patials and view floder name change

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const hbs = require("hbs");
 
 // console.log(__dirname);
 // const staticPath = path.join(__dirname, "../public");
 // console.log(staticPath);
+const templatePath = path.join(__dirname, "../template/views");
+const partialsPath = path.join(__dirname, "../template/partials");
 
 // app.use(express.static(staticPath));
 
 // to setUp view engine
 app.set("view engine", "hbs");
+app.set("views", templatePath);
+
+hbs.registerPartials(partialsPath);
 
 app.get("/", (req, res) => {
   res.render("index.hbs", {
@@ -21,6 +27,6 @@ app.get("/", (req, res) => {
   res.send("home page");
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("listening");
 });
