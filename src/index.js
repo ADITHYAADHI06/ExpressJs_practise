33c7aecc1f70bd5edde632a214cd3f2ef2db9d3e
ADITHYAADHI06 <adishettygu102@gmail.com>
2022-04-14 14:03:34 +0530

    dynamic data adding to website

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ const app = express();
 const path = require("path");
 
 // console.log(__dirname);
-const staticPath = path.join(__dirname, "../public");
+// const staticPath = path.join(__dirname, "../public");
 // console.log(staticPath);
 
-app.use(express.static(staticPath));
+// app.use(express.static(staticPath));
+
+// to setUp view engine
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index.hbs", {
+    name: "ram",
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("home page");
